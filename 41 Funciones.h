6377f0de3c4b8bde02e95f2cd7813ec64c35d2c1
serna <serna@192.168.1.70>
2021-01-20 14:33:43 -0600

    Funsiones h (18/Ene/2020)

---
+++ 41 Funciones.h
@@ -0,0 +1,23 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int suma (int x, int y){
+    return x+y;
+}
+int multi (int x, int y){
+    return x*y;
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCIONES_H */
+
